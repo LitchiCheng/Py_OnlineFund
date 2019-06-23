9e5fdd90abdf022817284d1ec26fd6fa36113f23
darboy <974782852@qq.com>
2019-06-23 13:11:16 +0800

    完成微博功能，但是不能屏蔽非正常情况如：违规内容等

--- py_AutoBlog.py
+++ py_AutoBlog.py
@@ -2,9 +2,28 @@ from selenium import webdriver
 import time,os,random,shutil
 from selenium.webdriver.support.ui import WebDriverWait
 
+class TimeOut:
+    def __init__(self, max_count, index):
+        self.__count = 0
+        self.__maxcount = max_count
+        self.__index = index
+    def trigger(self):
+        self.__count = self.__count+1
+        if self.__count >= self.__maxcount:
+            print(str(self.__index) + ' time out!')
+            self.__count = 0
+            return True
+        else:
+            return False
+
+trigger1 = TimeOut(10, 1)
+trigger2 = TimeOut(10, 2)
+trigger3 = TimeOut(10, 3)
+trigger4 = TimeOut(10, 4)
+
 def make_folder(path):
-    path=path.strip()
-    path=path.rstrip("\\")
+    # path=path.strip()
+    # path=path.rstrip("\\")
     isExists=os.path.exists(path)
     if not isExists:
         os.makedirs(path) 
@@ -23,24 +42,26 @@ def del_file(path):
         return False
 
 def moveFile(fileDir,tarDir):
-    while True:
-        pathDir = os.listdir(fileDir)    #取图片的原始路径
-        # rate=0.1    #自定义抽取图片的比例，比方说100张抽10张，那就是0.1
-        picknumber=9 #按照rate比例从文件夹中取一定数量图片
-        sample = random.sample(pathDir, picknumber)  #随机选取picknumber数量的样本图片
-        str_arrary = '#搞笑# #搞笑动图# #幽默# #笑话#\n'
-        index = 0
-        for i in sample:
-            file_name, extension = os.path.splitext(i)
-            index = index + 1
-            str_arrary = str_arrary + str(index) + '. ' + file_name + '\n'
-        print(str_arrary)
-        for name in sample:
-            try:
-                shutil.move(fileDir+name, tarDir+name)
-            except Exception as e:
-                print(e)
-        return str_arrary
+    # if trigger1.trigger():
+    #     break
+    pathDir = os.listdir(fileDir)    #取图片的原始路径
+    picknumber=9 #按照rate比例从文件夹中取一定数量图片
+    sample = random.sample(pathDir, picknumber)  #随机选取picknumber数量的样本图片
+    str_arrary = '#搞笑# #搞笑动图# #幽默# #笑话#\n'
+    index = 0
+    for i in sample:
+        file_name, extension = os.path.splitext(i)
+        index = index + 1
+        str_arrary = str_arrary + str(index) + '. ' + file_name + '\n'
+    print(str_arrary)
+    for name in sample:
+        try:
+            shutil.move(fileDir+'\\'+name, tarDir+'\\'+name)
+        except Exception as e:
+            print(e)
+        # else:
+        #     if len(os.listdir(tarDir)) == 9:
+    return str_arrary
 
 def checkFileExist(path):           #判断该路径下是否有文件存在
     if(len(os.listdir(path)) > 0):
@@ -79,9 +100,9 @@ class AutoBlog:
     def inputUserAndPassword(self,user_name,user_password,wait_time,log_in_time):
         try:
             WebDriverWait(self.browser, wait_time).until(lambda x: x.find_element_by_xpath('//*[@id="loginname"]')).send_keys(user_name)#'darboy@foxmail.com'
-            self.browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[3]/div[2]/div/input').send_keys(user_password)#'3.14159265758'
+            WebDriverWait(self.browser, wait_time).until(lambda x: x.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[3]/div[2]/div/input')).send_keys(user_password)#'3.14159265758'
             time.sleep(1)
-            self.browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[3]/div[6]/a').click()
+            WebDriverWait(self.browser, wait_time).until(lambda x: x.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[3]/div[6]/a')).click()
             time.sleep(5)
         except:
             pass
@@ -90,28 +111,49 @@ class AutoBlog:
         time.sleep(log_in_time) #15
 
     def sendBlog(self,pic_source,pic_target,script_path,upload_time):
-        del_file(pic_target)#'D:\\uploadImage'
-        make_folder(pic_target)   #'D:\\uploadImage'
-        while True:
-            if(os.path.exists(pic_target)):
-                if not checkFileExist(pic_target):
-                    text = moveFile(pic_source,pic_target)   #'D:\\爬虫下载\\image\\','D:\\uploadImage\\'
-                    time.sleep(1)
-                    break
+        if(os.path.exists(pic_target)):
+            if not checkFileExist(pic_target):
+                text = moveFile(pic_source,pic_target)   #'D:\\爬虫下载\\image\\','D:\\uploadImage\\'
+                time.sleep(1)
             else:
-                del_file(pic_target)#'D:\\uploadImage'
-                make_folder(pic_target)   #'D:\\uploadImage'
+                del_file(pic_target)
+                make_folder(pic_target)
+                text = 'NOTHING'
+                text = moveFile(pic_source,pic_target)   #'D:\\爬虫下载\\image\\','D:\\uploadImage\\'
+                time.sleep(1)
+        else:
+            # del_file(pic_target)#'D:\\uploadImage'
+            make_folder(pic_target)   #'D:\\uploadImage'
+            text = 'NOTHING'
+            text = moveFile(pic_source,pic_target)   #'D:\\爬虫下载\\image\\','D:\\uploadImage\\'
+            time.sleep(1)
                 
         self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[2]/textarea').send_keys(text)
         time.sleep(1)
         self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[3]/div[2]/a[2]').click()    #图片
+        time.sleep(2)
         os.system(script_path)#"E:\\code\\python\\py-FundOnline\\upload.exe"
         time.sleep(upload_time)#20
         self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[3]/div[1]/a').click()       #发布
+        try:
+            error = self.browser.find_elements_by_class_name('S_txt1')
+        except:
+            error = 0
+            print('没找到')
+        if error:
+            try:
+                self.browser.find_element_by_xpath('/html/body/div[10]/div[2]/div[4]/a').click()
+                time.sleep(2)
+                self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[2]/textarea').clear()
+                self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[2]/textarea').send_keys('#搞笑# #搞笑动图# #幽默# #笑话#')
+                self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[3]/div[1]/a').click()       #发布
+            except:
+                print('有问题')
+
 
 blog=AutoBlog()
 blog.openTheBlog("https://weibo.com",10)
 blog.inputUserAndPassword('darboy@foxmail.com', '3.14159265758', 10, 10)
 while True:
-    blog.sendBlog('D:\\爬虫下载\\image\\','D:\\uploadImage\\',"E:\\code\\python\\py-FundOnline\\upload.exe",20)
+    blog.sendBlog('D:\\爬虫下载\\image','D:\\uploadImage',"E:\\code\\python\\py-FundOnline\\upload.exe",20)
     time.sleep(300) 
